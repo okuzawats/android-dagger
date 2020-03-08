12b3d124415a8f89523bbb07be04ee52710db4da
okuzawats <okuzawats@gmail.com>
2020-03-08 21:42:12 +0900

    AppComponentのメンバ宣言

--- app/src/main/java/com/okuzawats/dagger/App.kt
+++ app/src/main/java/com/okuzawats/dagger/App.kt
@@ -2,13 +2,17 @@ package com.okuzawats.dagger
 
 import android.app.Application
 import android.util.Log
+import com.okuzawats.dagger.di.AppComponent
 import com.okuzawats.dagger.di.DaggerAppComponent
 
 class App : Application() {
+
+    lateinit var appComponent: AppComponent
+
     override fun onCreate() {
         super.onCreate()
 
-        val appComponent = DaggerAppComponent.create()
+        appComponent = DaggerAppComponent.create()
         val luckyNumber = appComponent.getLuckyNumberText()
         Log.i("App", luckyNumber)
     }
