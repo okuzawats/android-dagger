e09bef148983a3ea57d1041f6108cbd073e5959e
okuzawats <okuzawats@gmail.com>
2020-03-08 21:31:40 +0900

    AppComponent#getLuckyNumberTextを定義

--- app/src/main/java/com/okuzawats/dagger/di/AppComponent.kt
+++ app/src/main/java/com/okuzawats/dagger/di/AppComponent.kt
@@ -1,6 +1,10 @@
 package com.okuzawats.dagger.di
 
 import dagger.Component
+import javax.inject.Named
 
 @Component(modules = [LuckyNumberModule::class])
-interface AppComponent
+interface AppComponent {
+    @Named("lucky number text")
+    fun getLuckyNumberText(): String
+}
