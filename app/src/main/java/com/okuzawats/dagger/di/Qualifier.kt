b6c9436087bb6cc5e54a3b563d3e3b40e5c584e3
okuzawats <okuzawats@gmail.com>
2020-03-14 09:38:19 +0900

    アノテーションを定義

---
+++ app/src/main/java/com/okuzawats/dagger/di/Qualifier.kt
@@ -0,0 +1,12 @@
+package com.okuzawats.dagger.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+annotation class NamedLuckyNumberText
+
+@Qualifier
+annotation class NamedLuckyNumber
+
+@Qualifier
+annotation class NamedFortuneText
