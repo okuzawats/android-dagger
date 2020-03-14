eddc1c9fdb30e4a85fb67497ea7a9f6336f31d97
okuzawats <okuzawats@gmail.com>
2020-03-14 09:19:47 +0900

    AppComponent#getFortuneTextを作成

--- app/src/main/java/com/okuzawats/dagger/di/AppComponent.kt
+++ app/src/main/java/com/okuzawats/dagger/di/AppComponent.kt
@@ -4,8 +4,14 @@ import dagger.Component
 import javax.inject.Named
 
 @AppScope
-@Component(modules = [LuckyNumberModule::class])
+@Component(modules = [
+    LuckyNumberModule::class,
+    FortuneModule::class
+])
 interface AppComponent {
     @Named("lucky number text")
     fun getLuckyNumberText(): String
+
+    @Named("fortune text")
+    fun getFortuneText(): String
 }
